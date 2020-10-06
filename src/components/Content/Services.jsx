52597df3b96660e3e098aa75fff17afc17ecdde7
Brian Li <antai0926@gmail.com>
2020-10-06 17:14:55 +0800

    add the services part

--- src/components/Content/Services.jsx
+++ src/components/Content/Services.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import LaptopImg1 from '../../assets/img/service-laptop-1.webp';
+import LaptopImg2 from '../../assets/img/service-laptop-2.webp';
 
 const Services = () => {
   const Container = styled.div`
-    padding: 30px 0;
+    padding: 40px 0 60px 0;
     display: flex;
     flex-direction: column;
+    gap: 40px;
     align-items: center;
     background-color: #dde7ed;
+    font-family: roboto-bold, roboto, sans-serif;
   `;
   const Title = styled.div`
     color: black;
@@ -17,21 +21,60 @@ const Services = () => {
   `;
   const Row = styled.div`
     display: flex;
+    flex-wrap: ${(props) => (props.reverse ? 'wrap-reverse' : 'wrap')};
     justify-content: center;
+    align-items: center;
+    gap: 100px;
+    padding: 0 10px;
+    @media (max-width: 1040px) {
+      gap: 20px;
+    }
   `;
   const Element = styled.div``;
+  const Img = styled.img`
+    @media (max-width: 400px) {
+      width: 95vw;
+    }
+  `;
+  const ElTitle = styled.div`
+    color: #292929;
+    font-size: 23px;
+    margin-bottom: 10px;
+  `;
+  const Description = styled.div`
+    color: black;
+    font-size: 16px;
+  `;
   return (
     <Container>
       <Row>
         <Title>Our Services</Title>
       </Row>
       <Row>
-        <Element></Element>
-        <Element></Element>
+        <Element>
+          <Img src={LaptopImg1} alt="LaptopImg1" />
+        </Element>
+        <Element>
+          <ElTitle>Consulting</ElTitle>
+          <Description>
+            We strategize, design, build and implement omni-channel B2C and B2B
+            <br />
+            commerce capabilities, platforms and systems.
+          </Description>
+        </Element>
       </Row>
-      <Row>
-        <Element></Element>
-        <Element></Element>
+      <Row reverse>
+        <Element>
+          <ElTitle>IT Implementation</ElTitle>
+          <Description>
+            We strategize, design, build and implement omni-channel B2C and B2B
+            <br />
+            commerce capabilities, platforms and systems.
+          </Description>
+        </Element>
+        <Element>
+          <Img src={LaptopImg2} alt="LaptopImg2" />
+        </Element>
       </Row>
     </Container>
   );
